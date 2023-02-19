57bafa3e938cf2be8c6f2ca474df949721d7bcda
robonene <nene.bot@icloud.com>
2023-02-19 18:26:02 -0500

    general structure

---
+++ lenbot.py
@@ -0,0 +1,19 @@
+import discord, ssl
+from discord.ext import commands
+
+TOKEN = "**Token Goes Here**"
+GUILD = "myServer"
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    await message.channel.send("hi!")
+
+bot.run(TOKEN)
